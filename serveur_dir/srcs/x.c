5e052be482cc22e1ce3c31d072363b28e0c3b9fd
gmp89 <gm.petrov@yahoo.fr>
2014-05-21 22:31:14 +0200

    update

--- serveur_dir/srcs/x.c
+++ serveur_dir/srcs/x.c
@@ -6,7 +6,7 @@
 /*   By: gpetrov <gpetrov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/21 22:16:05 by gpetrov           #+#    #+#             */
-/*   Updated: 2014/05/21 22:18:12 by gpetrov          ###   ########.fr       */
+/*   Updated: 2014/05/21 22:26:48 by gpetrov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,23 @@
 #include <string.h>
 #include "serveur.h"
 
-int		x_int(int err, int res, char *str, char *file, int line)
+int		x_int(int err, int res, char *file, int line)
 {
 	if (res == err)
 	{
-		fprintf(stderr, "%s error (%s, %d): %s\n",
-				str, file, line, strerror(errno));
+		fprintf(stderr, "error (%s, %d): %s\n",
+				file, line, strerror(errno));
 		exit (1);
 	}
 	return (res);
 }
 
-void	*x_void(void *err, void *res, char *str, char *file, int line)
+void	*x_void(void *err, void *res, char *file, int line)
 {
 	if (res == err)
 	{
-		fprintf(stderr, "%s error (%s, %d): %s\n",
-				str, file, line, strerror(errno));
+		fprintf(stderr, "error (%s, %d): %s\n",
+				file, line, strerror(errno));
 		exit (1);
 	}
 	return (res);
