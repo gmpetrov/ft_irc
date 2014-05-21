2d50081d150f39c75b30003036ce342abba141b0
gmp89 <gm.petrov@yahoo.fr>
2014-05-21 23:51:51 +0200

    nickname OK

--- client_dir/includes/client.h
+++ client_dir/includes/client.h
@@ -6,7 +6,7 @@
 /*   By: gpetrov <gpetrov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/13 18:16:38 by gpetrov           #+#    #+#             */
-/*   Updated: 2014/05/21 21:53:16 by gpetrov          ###   ########.fr       */
+/*   Updated: 2014/05/21 23:20:57 by gpetrov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@
 /*
 **	tools.c
 */
-void	action(int sock);
+void	free_tab(char ***tab);
 void	error_exit(char *str);
 /*
 **	read_write_server.c
@@ -43,8 +43,8 @@ void	write_server(int sock, char *buf);
 **	main.c
 */
 int		output(int sock);
-void	input(int sock);
+void	input(int sock, char **login);
 int		create_client(char *addr, int port);
-void	free_tab(char ***tab);
+void	action(int sock, char **env);
 
 #endif
--- client_dir/srcs/main.c
+++ client_dir/srcs/main.c
@@ -6,20 +6,54 @@
 /*   By: gpetrov <gpetrov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/13 18:17:46 by gpetrov           #+#    #+#             */
-/*   Updated: 2014/05/21 21:55:47 by gpetrov          ###   ########.fr       */
+/*   Updated: 2014/05/21 23:50:17 by gpetrov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include "client.h"
 
-void	input(int sock)
+void	send_id(int sock, char **login)
+{
+	char	*s1;
+	char	*s2;
+
+	s1 = ft_strdup("\033[32m[");
+	s2 = ft_strdup(*login);
+	s1 = ft_strcat(s1, s2);
+	free(s2);
+	s2 = ft_strdup("] said \033[0m\n");
+	s1 = ft_strcat(s1, s2);
+	write_server(sock, s1);
+	free(s1);
+	free(s2);
+}
+
+void	input(int sock, char **login)
 {
 	char	*line;
+	char	**tab;
 
 	get_next_line(STDIN_FILENO, &line);
-	write_server(sock, line);
+	if (ft_strncmp(line, "/nick", 5) == 0)
+	{
+		tab = ft_strsplit(line, ' ');
+		if (tab[1] && !tab[2] && (ft_strlen(tab[1]) <= 9))
+		{
+			free(*login);
+			*login = ft_strdup(tab[1]);
+		}
+		else
+			write(1, "\033[31mWrong argument\033[0m\n", 24);
+		free_tab(&tab);
+	}
+	else
+	{
+		send_id(sock, login);
+		write_server(sock, line);
+	}
 	free(line);
 }
 
@@ -34,26 +68,82 @@ int		output(int sock)
 		ft_putstr("\033[31mServer disconnected\033[0m\n");
 		return (-1);
 	}
+	write(1, "\n", 1);
 	ft_putstr(buf);
 	write(1, "\n", 1);
 	return (0);
 }
 
-void	action(int sock)
+char	*get_name(char **env)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	*login;
+
+	i = 0;
+	k = 0;
+	while (env[i])
+	{
+		if (strncmp(env[i], "LOGNAME", 7) == 0)
+		{
+			j = 8;
+			login = (char *)malloc(sizeof(char) * ft_strlen(env[i]));
+			while (env[i][j] != 0)
+			{
+				login[k] = env[i][j];
+				k++;
+				j++;
+			}
+			login[k] = 0;
+			return (login);
+		}
+		i++;
+	}
+	return ("Default");
+}
+
+void	prompt(char *login)
+{
+	ft_putstr("\033[33m");
+	ft_putstr("[");
+	ft_putstr(login);
+	ft_putstr("]");
+	ft_putstr("-> \033[0m");
+}
+
+char	*joined_the_room(char *login)
+{
+	char	*s1;
+	char	*s2;
+
+	s1 = ft_strdup(login);
+	s2 = ft_strdup(" joined the room");
+	s1 = ft_strcat(s1, s2);
+	free(s2);
+	return (s1);
+}
+
+void	action(int sock, char **env)
 {
 	fd_set	readfd;
 	int		ret;
+	char	*name;
+	char	*join;
 
-	write_server(sock, "New Client Connected");
+	name = get_name(env);
+	join = joined_the_room(name);
+	write_server(sock, join);
 	while (42)
 	{
+		prompt(name);
 		FD_ZERO(&readfd);
 		FD_SET(STDIN_FILENO, &readfd);
 		FD_SET(sock, &readfd);
 		if ((ret = select(sock + 1, &readfd, NULL, NULL, NULL)) < 0)
 			error_exit("select() problem");
 		if (FD_ISSET(STDIN_FILENO, &readfd))
-			input(sock);
+			input(sock, &name);
 		else if (FD_ISSET(sock, &readfd))
 		{
 			if (output(sock) == -1)
@@ -62,7 +152,7 @@ void	action(int sock)
 	}
 }
 
-int		main(int ac, char **av)
+int		main(int ac, char **av, char **env)
 {
 	int		port;
 	int		sock;
@@ -74,7 +164,7 @@ int		main(int ac, char **av)
 	}
 	port = ft_atoi(av[2]);
 	sock = create_client(av[1], port);
-	action(sock);
+	action(sock, env);
 	close(sock);
 	return (0);
 }
